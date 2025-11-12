47090d5a8de45f8826708cb8454eeb4f385d48b4
Luis Edgardo Duno Castellano <luisdunocastellano@gmail.com>
2025-11-11 21:34:49 -0400

    update style product

--- app/main.js
+++ app/main.js
@@ -37,7 +37,7 @@ function App(){
   
       mainWindow.loadFile(path.join(__dirname,'index.html'));
 
-    //mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
 
   caja_control("Apertura",mainWindow)
